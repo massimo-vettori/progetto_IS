1db685b78511a6ab6e752773b5a93636e8d625b0
Chiara Pareschi <996523@stud.unive.it>
2022-11-06 11:43:11 +0100

    🐛 BugFix - in RegisterFragment txt_password was always the same as txt_confirm_password, in themes.xml substituted #fff with @color/white,  in RegisterFragment, added check of checkBox for privacy, added names in strings.xml

--- app/src/main/java/com/lacliquep/barattopoli/fragments/sign/RegisterFragment.java
+++ app/src/main/java/com/lacliquep/barattopoli/fragments/sign/RegisterFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -44,6 +45,8 @@ public class RegisterFragment extends Fragment {
     EditText sign_up_password_confirm_field;
     //register button
     Button sign_up_button;
+    //check box
+    CheckBox check_box;
 
     //get an instance of the FirebaseAuth
     private final FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -55,11 +58,13 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        // get the reference of Buttons and text fields
+        // get the reference of Buttons, text fields and check box
         sign_up_email_field = view.findViewById(R.id.sign_up_email_field);
         sign_up_password_field = view.findViewById(R.id.sign_up_password_field);
         sign_up_password_confirm_field = view.findViewById(R.id.sign_up_password_confirm_field);
         sign_up_button = view.findViewById(R.id.sign_up_button);
+        // TODO add privacy form
+        check_box = view.findViewById(R.id.check_box);
 
         // called whenever the button register is clicked
         // it checks:
@@ -74,9 +79,9 @@ public class RegisterFragment extends Fragment {
             //the text inserted in the password field
             String txt_password = sign_up_password_field.getText().toString();
             //the text inserted in the confirm password field
-            String txt_confirm_password = sign_up_password_field.getText().toString();
+            String txt_confirm_password = sign_up_password_confirm_field.getText().toString();
+            boolean checked = check_box.isChecked();
 
-            //TODO:add checkbox control
             if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_confirm_password)) {
                 if (TextUtils.isEmpty(txt_email) && TextUtils.isEmpty(txt_password)) {
                     Toast.makeText(getActivity(), getString(R.string.email) + " , " + getString(R.string.password) + " , " + getString(R.string.password_confirm) + " , " + getString(R.string.empty_text), Toast.LENGTH_SHORT).show();
@@ -94,12 +99,19 @@ public class RegisterFragment extends Fragment {
             } else {
                 if (txt_password.length() < 6) {
                     Toast.makeText(getActivity(), getString(R.string.error_password), Toast.LENGTH_SHORT).show();
-                } else {//everything seems to be ok, so call the method for the actual registration
-                    registerUser(txt_email, txt_password);
-
+                } else {
+                    if (!(txt_password.equals(txt_confirm_password))) {
+                        Toast.makeText(getActivity(), getString(R.string.password) + ", " + getString(R.string.password_confirm) + getString(R.string.match_error), Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (!checked) {
+                            Toast.makeText(getActivity(), getString(R.string.select_error) + getString(R.string.accept_privacy), Toast.LENGTH_LONG).show();
+                        } else {
+                            //everything seems to be ok, so call the method for the actual registration
+                            registerUser(txt_email, txt_password);
+                        }
+                    }
                 }
             }
-            //TODO: after registration do login and then start signActivity
         });
 
         return view;
@@ -122,10 +134,9 @@ public class RegisterFragment extends Fragment {
                 //login of user
                 loginUser(email, password);
             } else {
-                //TODO: delete exception
                 String error = (Objects.requireNonNull(task.getException()).getMessage());
                 //negative feedback
-                Toast.makeText(getActivity(), getString(R.string.Registration) + getString(R.string.failure) + ":\n" + error, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), getString(R.string.Registration) + getString(R.string.failure) + ": \n" + error, Toast.LENGTH_SHORT).show();
             }
 
         });
@@ -139,7 +150,7 @@ public class RegisterFragment extends Fragment {
     private void loginUser(String email, String password) {
         auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
             Toast.makeText(getActivity(), getString(R.string.Login) + getString(R.string.success), Toast.LENGTH_SHORT).show();
-            //TODO: after login start SignActivity
+            //TODO: after login, start SignActivity
             startActivity(new Intent(getActivity(), SignActivity.class));
         });
     }
