24c6d6b643510808c1f05d36bef22aa2291ca653
giacomofb <883914@stud.unive.it>
2022-11-19 17:28:25 +0100

    Finish Navigation drawer

--- app/src/main/java/com/lacliquep/barattopoli/MainActivity.java
+++ app/src/main/java/com/lacliquep/barattopoli/MainActivity.java
@@ -1,16 +1,23 @@
 package com.lacliquep.barattopoli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
+import com.lacliquep.barattopoli.fragments.ObjectFragment;
+import com.lacliquep.barattopoli.fragments.ServicesFragment;
 import com.lacliquep.barattopoli.views.ItemView;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -22,11 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar); // set our ActionBar
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);// for create hamburger for open the NavigationaBar
         drawer.addDrawerListener(toggle);
         toggle.syncState(); // rotating the hamburger icon
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ObjectFragment()).commit(); // this start for the first when you open this activity
+            navigationView.setCheckedItem(R.id.nav_object);
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_object:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ObjectFragment()).commit();
+                break;
+            case R.id.nav_services:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ServicesFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/lacliquep/barattopoli/fragments/ObjectFragment.java
@@ -0,0 +1,20 @@
+package com.lacliquep.barattopoli.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.lacliquep.barattopoli.R;
+
+public class ObjectFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_object, container, false);
+    }
+}
