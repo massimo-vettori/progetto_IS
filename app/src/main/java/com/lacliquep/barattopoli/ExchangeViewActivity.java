efdf2703bdaf88ca2ae1a774eda2645154cb5ff3
massimo-vettori <884477@stud.unive.it>
2022-12-28 19:19:42 +0100

    Added xml to display current active exchanges
     + New xml to display a single exchange
     + New xml to display a list of exchanges objects
    
    Co-Authored-By: Chiara Pareschi <78914510+ChiaraPareschi@users.noreply.github.com>
    Co-Authored-By: giacomofb <73796564+giacomofb@users.noreply.github.com>

---
+++ app/src/main/java/com/lacliquep/barattopoli/ExchangeViewActivity.java
@@ -0,0 +1,14 @@
+package com.lacliquep.barattopoli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExchangeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_view);
+    }
+}
---
+++ app/src/main/java/com/lacliquep/barattopoli/views/ExchangeItemView.java
@@ -0,0 +1,22 @@
+package com.lacliquep.barattopoli.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class ExchangeItemView extends View {
+
+    public ExchangeItemView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    protected void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+
+    }
+
+}
